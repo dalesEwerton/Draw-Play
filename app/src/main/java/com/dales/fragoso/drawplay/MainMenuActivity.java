6fdf3fcc627de8674f09a53a5f2264e4560fd909
Dales Fragoso <dales.fragoso@ccc.ufcg.edu.br>
2017-07-07 15:54:09 -0300

    Criando o menu inicial

--- app/src/main/java/com/dales/fragoso/drawplay/MainMenuActivity.java
+++ app/src/main/java/com/dales/fragoso/drawplay/MainMenuActivity.java
@@ -1,7 +1,12 @@
 package com.dales.fragoso.drawplay;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        android.support.v7.app.ActionBar bar = getSupportActionBar();
+
+        bar.hide();
+
+        View decorView = getWindow().getDecorView();
+
+        // Escondendo a barra de navegação e a barra de status .
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
+
 }
