9b5772a17fc2a7eca31b369cd431351f6cee091d
Dales Fragoso <dales.fragoso@ccc.ufcg.edu.br>
2017-07-30 17:15:56 -0300

    Modelando o cadastro de equipe

--- app/src/main/java/com/dales/fragoso/drawplay/View/RegisterActivity.java
+++ app/src/main/java/com/dales/fragoso/drawplay/View/RegisterActivity.java
@@ -1,25 +1,18 @@
 package com.dales.fragoso.drawplay.View;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.dales.fragoso.drawplay.R;
 
-public class TeamsActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teams);
+        setContentView(R.layout.activity_register);
 
         android.support.v7.app.ActionBar bar = getSupportActionBar();
         bar.hide();
-    }
-
-    public void register(View view) {
-        Intent it = new Intent(TeamsActivity.this, RegisterActivity.class);
-        startActivity(it);
     }
 }
